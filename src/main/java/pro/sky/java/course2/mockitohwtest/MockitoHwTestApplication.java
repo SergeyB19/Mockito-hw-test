fe5b844ff356d0541a371e2d159c836e8ea8ca28
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-28 10:28:16 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/mockitohwtest/MockitoHwTestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.mockitohwtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoHwTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoHwTestApplication.class, args);
+	}
+
+}
